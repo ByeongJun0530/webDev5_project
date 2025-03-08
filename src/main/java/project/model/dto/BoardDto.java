48ab37370a63f7c26d26bba1c4151a49bff1f113
ByeongJun0530 <dynamicpurs@gmail.com>
2025-03-08 20:26:23 +0900

    03/08

--- src/main/java/project/model/dto/BoardDto.java
+++ src/main/java/project/model/dto/BoardDto.java
@@ -11,9 +11,9 @@ public class BoardDto {
     private String btitle; // 게시물 제목
     private String bcontent; // 게시물 내용
     private int mno; // 회원번호 fk
+    private int mid; // 회원id fk
     private int cno; // 카테고리 번호 fk
+    private String cname; // 카테고리 이름 fk
     private LocalDate cdate; // 등록일자
     private LocalDate mdate; // 수정일자
-    private int mid; // 회원id fk
-    private String cname; // 카테고리 이름 fk
 }
